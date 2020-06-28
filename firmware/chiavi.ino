855cd01a05b12cea67dfde43966116f0bd1c1867
Biel <bielp07@gmail.com>
2020-06-28 16:44:18 -0300

    rename everything

---
+++ firmware/chiavi.ino
@@ -0,0 +1,11 @@
+/*
+ * chiavi - bap.re
+ * 
+ * 6 keys chorded keyboard.
+ *
+ * source: https://github.com/bapresearch/chiavi
+ * website: https://bap.re/chiavi
+ *
+ */
+
+
--- firmware/teclas.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * teclas - bap.re
- * 
- * 6 keys chorded keyboard.
- *
- * source: https://github.com/bapresearch/teclas
- * website: https://bap.re/teclas
- *
- */
